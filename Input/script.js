fd2fea0ba945e0b49df3faff3ef25c20b4108851
Saurabh <idkxyz001@gmail.com>
2026-03-30 11:55:00 +0530

    Input Event

---
+++ Input/script.js
@@ -0,0 +1,69 @@
+const input = document.querySelector("input")
+const button = document.querySelector("button")
+const h1 = document.querySelector("#main")
+
+
+// console.log(input.type);
+// console.log(input.name);
+// console.log(input.id);
+// console.log(input.placeholder);
+// console.log(input.required);
+// console.log(input.value);
+
+
+// button.addEventListener("click", () => {
+
+//     if (input.value.trim() === "") {
+//         input.value = ""
+//         return;
+//     }
+
+//     console.log(input.value)
+//     console.log(h1.innerText);
+//     h1.innerText = "Hello " + input.value + "!";
+//     input.value = ""
+
+// })
+
+// input.type = "color"
+
+
+// button.addEventListener("click", () => {
+//     console.log("Color");
+
+//     document.body.style.backgroundColor = input.value
+// })
+
+
+// input.type = "color"
+// input.addEventListener("input", () => {
+//     document.body.style.backgroundColor = input.value
+//     // console.log("Input..");
+//     // h1.innerText = "Hello " + input.value + "!";
+// })
+
+// input.type = "color"
+// input.addEventListener("change", () => {
+//     document.body.style.backgroundColor = input.value
+//     // console.log(input.value);
+//     // h1.innerText = "Hello " + input.value + "!";
+// })
+
+// input.type = "color"
+// input.addEventListener("focus", () => {
+//     // document.body.style.backgroundColor = input.value
+//     console.log(input.value);
+//     // h1.innerText = "Hello " + input.value + "!";
+// })
+// input.addEventListener("blur", () => {
+//     // document.body.style.backgroundColor = input.value
+//     console.log(input.value);
+//     // h1.innerText = "Hello " + input.value + "!";
+// })
+
+
+// input.addEventListener("input", (e) => {
+//     // document.body.style.backgroundColor = input.value
+//     console.log("Input..", e.target);
+//     h1.innerText = "Hello " + e.target.value + "!";
+// })
