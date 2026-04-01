e4cff4e8440a0dad2007b51ad36700a7886f2253
Saurabh <idkxyz001@gmail.com>
2026-04-01 15:09:57 +0530

    JS

---
+++ 02-04/01-04-26/script.js
@@ -0,0 +1,59 @@
+//int float double char string boolean
+//Number           String           Boolean
+
+//int Name = 12;
+//  Age = 12;
+
+//var let const
+
+// var name = "Easyskill"
+// name = "Hiii"
+// console.log(name);
+// let name = "Easyskill"
+// name = "Hii"
+// let name;
+
+
+// const name = "Easyskill"
+
+// const name;
+
+
+// let 1age;
+// let @ge;
+// let user name; user_name
+
+// let age = 12;
+// let PI = 3.14;
+// let name = "Easyskill"
+// let isLoggedIn = true;
+
+//+ - * / %
+
+// console.log(12 % 2)
+
+// console.log(238463485 % 10);//5
+// console.log(238463485 % 100);//85
+// console.log(238463485 % 1000);//485
+// console.log(238463485 % 1000000000);//238463485
+
+
+let a = 12;
+let b = a;
+
+// a++;
+// ++a;
+// a--;
+// --a;
+
+// a = a + 10;
+// a = a - 15;
+// a = a * 2;
+// a = a / 2;
+
+// a += 10;
+// a -= 15;
+// a *= 2;
+// a /= 2;
+console.log(a)
+console.log(b)
---
+++ 02-04/31-03-26/script.js
@@ -0,0 +1,3 @@
+console.log("Hello User")
+console.warn("Hello User")
+console.error("Hello User")
