b3903d120df51600d42acca51bd7f22ee836a466
Saurabh <idkxyz001@gmail.com>
2026-04-06 15:07:27 +0530

    Loop

---
+++ 02-04 batch/06-04-26/script.js
@@ -0,0 +1,87 @@
+//AND(&&) OR(||) NOT(!)
+
+//AND && operator returns true if both operands are true, otherwise it returns false.
+
+//Ture and False
+
+// console.log(true && true)  // ->true
+// console.log(true && false) //->false
+// console.log(false && true) //->false
+// console.log(false && false)//->false
+
+// console.log(true || true)  // ->true
+// console.log(true || false) //->true
+// console.log(false || true) //->true
+// console.log(false || false)//->false
+
+// console.log(!true) // -> false
+// console.log(!false)// ->true
+
+
+//+ - ++ --
+
+// let a = -20;
+// console.log(a)
+// console.log(+a)
+// console.log(-a)
+// a = a + 25;
+// a += 25
+
+// let a = 10;
+// let b = a++;//a=11 , b=10;
+// let c = ++a;//a = 12 , c = 12;
+
+
+// a++ , ++a
+// a--, --a
+
+// a++;
+// ++a;
+
+// console.log(a)
+
+
+//WHILE , DO WHILE, FOR
+// let i = 0
+
+// while (i <= 10) {
+//     console.log("Hii", i)
+//     i++;
+// }
+
+// console.log(i);
+
+// let num = 384632;
+
+
+// while (num != 0) {
+
+//     console.log("Hello", num)
+//     num = parseInt(num / 10)
+// }
+// let i = 20;
+// do {
+//     console.log("Hii", i)
+//     i++;
+// } while (i <= 10)
+
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log("Hii", i)
+// }
+
+
+// let num = Number(prompt("Enter a Number"))
+// let rev = 0;
+// let sum = 0;
+
+// while (num != 0) {
+//     let rem = num % 10;
+//     sum = sum+rem;
+//     // rev = rev * 10 + rem
+//     num = parseInt(num / 10)
+// }
+
+//3248632 :- 22
+//3248632 :- 6
+//3248632 :- 
