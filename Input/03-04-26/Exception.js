f7c1a6f13eec5b8674e359ec7019df091c26f9e2
Saurabh <idkxyz001@gmail.com>
2026-04-09 12:00:28 +0530

    Try-Catch

---
+++ Input/03-04-26/Exception.js
@@ -0,0 +1,40 @@
+// alert("Hello, World!");
+const obj = {
+    name: "MyName",
+    age: 34,
+    obj: {
+        key: "value",
+        data: "Set"
+    }
+}
+// console.log("Hello")
+
+// console.log(obj.name)
+// try {
+//     console.log(obj.obj.xyz.abcd)//Error
+// } catch (error) {
+//     console.error(error)
+// }
+
+// console.log("Next Line")
+
+// console.log(obj.obj?.xyz?.abcd)
+
+
+// try {
+//     console.log("Try Block")
+// } catch (error) {
+//     console.log("Catch Block")
+// } finally {
+//     console.log("finally block ")
+// }
+
+// console.log("Hii")
+
+// try {
+//     throw new Error("My Error")
+// } catch (error) {
+//     console.log("Error ", error)
+// }
+
+// console.log("Next Line")
