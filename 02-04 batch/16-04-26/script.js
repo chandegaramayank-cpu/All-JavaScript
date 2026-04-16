b58f60872e47fed94580de3bd5f462c46abc2efe
Saurabh <idkxyz001@gmail.com>
2026-04-16 15:35:56 +0530

    Array

---
+++ 02-04 batch/16-04-26/script.js
@@ -0,0 +1,67 @@
+// function process(id) {
+//     return new Promise((resolve, reject) => {
+//         console.log("mapping you for reaching home")
+//         setTimeout(() => {
+//             console.log(id)
+//             reject(id)
+//         }, 2000)
+//     })
+// }
+
+// process("way to parking")
+//     .then(() => {
+//         process("start the car")
+//             .then(() => {
+//                 process("way to lp savani school")
+//                     .then(() => {
+//                         process("take left for 500mtrs")
+//                             .then(() => {
+//                                 process("Take rights at gate no1")
+//                                     .then(() => {
+//                                         process("reached home 👍🏻")
+//                                     })
+//                             })
+//                     })
+//             })
+//     })
+//     .catch(() => {
+//         console.log("wrong way")
+//     })
+
+
+// -----------------------------------------------------------------------------------------
+
+// https://pool-collision-da5.notion.site/JS-Arrays-fa3724fea8c04748a7a0a89a29b5182c?pvs=143
+
+// int arr[5] = {1, 2, 3, 4, 5};
+// const Username = "Easyskill"
+
+// let a = 5
+// let b = [1, 2, 3, 4, 5, 6, 7]
+// let c = Array(10)
+// let d = Array.of(1, 2, 4, 5)
+// let e = Array.from(Username)
+
+// console.log(Array.isArray(b))
+// console.log(Array.isArray(a))
+
+// console.log(b[3])
+// console.log(b[2])
+
+// b[4] = 101
+
+// b[b.length] = 99
+// b[b.length] = 102
+// b[7] = 99
+// b[8] = 102
+//500
+
+// console.log("Ans :", b[b[2]])
+
+
+// b.push(9999)
+// b.pop()
+
+// const arr = [1, 2, 3, 4, 5]
+
+// arr.unshift(0)
