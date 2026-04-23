bfab57f844c95ffe5571ae8ac2889d67793aeaf3
Saurabh <idkxyz001@gmail.com>
2026-04-23 15:33:29 +0530

    Objects in JS

---
+++ 02-04 batch/23-04-2026/index.js
@@ -0,0 +1,74 @@
+//Object
+
+// const User = ["Easyskill", 18, "Easyskill@gmail", "Easyskill@2024"]
+
+// const i = "name"
+
+// const User = {
+//     name: "Easyskill",
+//     age: 19,
+//     email: "Easyskill@gmail.com",
+//     password: "Easyskill@2024",
+// }
+
+// User.age = 29;
+// User.A = "B"
+// User["user name"] = "MyName"
+// User["0"] = 12;
+
+
+// delete User.A;
+
+
+// for (let key in User) {
+//     console.log(`${key} -> ${User[key]}`);
+// }
+
+// console.log(User["email"])
+// console.log(User.age)
+
+
+// console.log(User[i])
+
+
+// const password = "Easyskill@2024"
+
+
+// const User = {
+//     name: "Easyskill",
+//     age: 19,
+//     email: "Easyskill@gmail.com",
+//     name: "MyKey",
+//     age: 90,
+//     // password: password
+//     password
+// }
+
+// console.table(User)
+// // Object.keys(User)
+// // Object.values(User)
+// console.log(
+//     Object.entries(User));
+
+
+// const obj1 = { a: 1 };
+// const obj2 = { b: 2 };
+
+// const merged = Object.assign({}, obj1, obj2, { c: 3 });
+// console.log(merged);
+
+
+// const User = {
+//     name: "Easyskill",
+//     age: 19,
+//     email: "Easyskill@gmail.com",
+//     name: "MyKey",
+//     age: 90,
+//     password: "p"
+// }
+
+// User.password = "MyPass"
+
+// // Object.freeze(User)
+
+// Object.seal(User)
---
+++ 02-04 batch/23-04-2026/script.js
@@ -0,0 +1,45 @@
+// const a = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+// ]
+
+
+// const b = [
+//     [9, 8, 7],
+//     [6, 5, 4],
+//     [3, 2, 1]
+// ]
+
+
+// const arr = [Array(3), Array(3), Array(3)];
+
+// for (let i = 0; i < a.length; i++) {
+//     for (let j = 0; j < a.length; j++) {
+//         arr[i][j] = a[i][j] + b[i][j]
+//     }
+// }
+
+
+//Selection Sort Algorithm
+// let arr = [7, 1, 29, 9, 2, 6, 8, 3, 38, 102, 90, 144]
+// let x = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//     let minIndex = i;
+
+//     for (let j = i + 1; j < arr.length; j++) {
+//         if (arr[j] < arr[minIndex]) {
+//             minIndex = j;
+//         }
+//         x++;
+//     }
+
+//     let temp = arr[i];
+//     arr[i] = arr[minIndex];
+//     arr[minIndex] = temp;
+
+// }
+
+// console.log(`Total comparisons: ${x}`);
+
